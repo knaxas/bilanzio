2e2c5426443c916829c525159125b42134262623
maxas <106425371+knaxas@users.noreply.github.com>
2026-08-02 20:12:06 +0200

    Refactor import of PrismaClient from prismaPkg

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -1,6 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import prismaPkg from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
+
+const { PrismaClient } = prismaPkg;
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
